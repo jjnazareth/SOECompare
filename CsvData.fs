b2db962215680de4bf89c73ac51101480263225c
jjnazareth <jivrajnazareth@gmail.com>
2024-08-20 12:47:13 +1000

    after .net 8.0

--- CsvData.fs
+++ CsvData.fs
@@ -1,38 +1,28 @@
 module CsvData
+
 open System.IO
 open FSharp.Data
 
-type Status = | Ok | Error
-type ExcelRow = { 
-    Diocese : string
-    Title: string
-    FirstName : string
-    LastName: string
-    EmailAddress: string
-    Status: Status
-    }
+type Status =
+    | Ok
+    | Error
+
+type ExcelRow =
+    { Diocese: string
+      Title: string
+      FirstName: string
+      LastName: string
+      EmailAddress: string
+      Status: Status }
 
 
-let getCsvData (path:string) =
-    let csv = CsvFile.Load(path)
+let getCsvData (path: string) =
+    let csv = CsvFile.Load(path, ",", '"', true, true)
     csv.Rows
-    |> Seq.map (fun row -> {
-        EmailAddress = row.[0] 
-        FirstName = row.[1]
-        LastName = row.[2]
-        Title = row.[3]
-        Diocese = row.[4]
-        Status = Ok
-        })
-
-
-
-
-
-
-
-
-
-
-
-
+    |> Seq.map (fun row ->
+        { EmailAddress = row.[0]
+          FirstName = row.[1]
+          LastName = row.[2]
+          Title = row.[3]
+          Diocese = row.[4]
+          Status = Ok })
--- Program.fs
+++ Program.fs
@@ -6,15 +6,15 @@ open FSharp.Collections
 
 let tee f x = f x; x
 
-let folder = @"C:\Users\Nazareth\Documents\Jivraj\SOE\Db"
-let fileName = @"Full Dbase 2021-04-28.xlsx"
+let folder = @"C:\Users\Jivraj\Documents\Jivraj\SOE\Db"
+let fileName = @"Full Dbase Open 2024-07-03.xlsx"
 let sheet = "Master"
 let xlFPath = Path.Combine (folder, fileName)
     
-let clnFName = @"cleaned_segment_export_5505166846.csv"
-let unsubFName = @"unsubscribed_segment_export_5505166846.csv"
-let subFName = @"subscribed_segment_export_a40494e639.csv"
-let ePrayerFName =  @"subscribed_segment_export_ccd077b9b7.csv"
+let clnFName = @"cleaned_email_segment_export_a4f09ca51a.csv"
+let unsubFName = @"unsubscribed_email_segment_export_a4f09ca51a.csv"
+let subFName = @"subscribed_email_segment_export_a0afb966c3.csv"
+let ePrayerFName =  @"eprayers_segment_export_41058b5d9b.csv"
 
 let noDbSubscribed =
     // subscribed in MailChimp but not in Full Dbase.xlsx
@@ -59,8 +59,30 @@ let dbUnsubscribed =
         select email
     }
 
+let dbCleaned =
+    // cleaned in MailChimp but showing in Full Dbase.xlsx
+    let path = Path.Combine (folder, clnFName)
+    let clnEmails  = 
+        query {
+            for data in getCsvData path do
+            select data.EmailAddress
+            } 
+        |> Seq.map (fun x -> x.Trim().ToLower())
+        |> Seq.toList
+
+    let dbEmails =
+        getExcelData xlFPath sheet
+        |> Seq.map (fun x -> x.EmailAddress.Trim().ToLower())
+        |> Seq.toList
+
+    query {
+        for email in dbEmails do 
+        where(List.contains email clnEmails)
+        select email
+    }
+
 let dbNotMailChimp =
-    // existing in Full Dbase.xlsx but not subscribed or unsubscribed in MailChimp
+    // existing in Full Dbase.xlsx but not subscribed or unsubscribed or cleaned in MailChimp
     let subEmails  = 
         let subPath = Path.Combine (folder, subFName)
         query {
@@ -80,23 +102,9 @@ let dbNotMailChimp =
         |> Seq.map (fun x -> x.Trim().ToLower())
         |> Seq.toList
 
-    let dbEmails =
-        getExcelData xlFPath sheet
-        |> Seq.map (fun x -> x.EmailAddress.Trim().ToLower())
-        |> Seq.toList
-
-    query {
-        for email in dbEmails do 
-        where ( (not <| List.contains email subEmails) &&
-                (not <| List.contains email unSubEmails) )
-               
-        select email
-    }
-
-let dbCleaned =
-    // cleaned in MailChimp but showing in Full Dbase.xlsx
     let path = Path.Combine (folder, clnFName)
-    let unSubEmails  = 
+
+    let clnEmails  = 
         query {
             for data in getCsvData path do
             select data.EmailAddress
@@ -111,7 +119,9 @@ let dbCleaned =
 
     query {
         for email in dbEmails do 
-        where(List.contains email unSubEmails)
+        where ( (not <| List.contains email subEmails) &&
+                (not <| List.contains email unSubEmails) &&  
+                (not <| List.contains email clnEmails)  )
         select email
     }
 
@@ -185,30 +195,29 @@ let newContactsCsv =
             
         select row
     }
+     
+let existingContactsCsv =
+    // existing in Full Dbase.xlsx and not unsubscribed in MailChimp
 
+    let unSubEmails  = 
+        let unsubPath = Path.Combine (folder, unsubFName)
+        query {
+            for data in getCsvData unsubPath do
+            select data.EmailAddress
+            } 
+        |> Seq.map (fun x -> x.Trim().ToLower())
+        |> Seq.toList
+
+    query {
+        for row in getExcelData xlFPath sheet do 
+        where  (not <| List.contains (row.EmailAddress.Trim().ToLower()) unSubEmails )            
+        select row
+    }
+     
 
 [<EntryPoint>]
 let main argv = 
-(*     getExcelAllData xlFPath sheet
-    |> Seq.filter (fun x -> x.EmailAddress.Trim() = "")
-    |> Seq.filter (fun x -> x.Address.Trim() = "")
-    |> Seq.filter (fun x -> x.Telephone <> "" || x.Mobile <> "")
-    |> Seq.length
 
-    |> (printfn "%i")
- 
-    getExcelAllData xlFPath sheet
-    |> Seq.filter (fun x -> x.EmailAddress.Trim() = "")
-    |> Seq.filter (fun x -> x.Address.Trim() <> "")
-    // |> Seq.filter (fun x -> x.Telephone <> "" || x.Mobile <> "")
-    |> Seq.length
-    |> (printfn "%i")
- 
-    getExcelAllData xlFPath sheet
-    |> Seq.filter (fun x -> x.EmailAddress <> "")
-    |> Seq.length
-    |> (printfn "%i")
- *) 
     if argv.Length = 0 then 
         printfn "Records that do not exist in Full Dbase.xlsx but are subscribed"
         noDbSubscribed
@@ -239,5 +248,6 @@ let main argv =
         printfn "Diocese, Email Address, Title, First Name, Last Name"
         newContactsCsv
         |> Seq.iter (fun x -> printfn "%s,%s,%s,%s,%s" x.Diocese (x.EmailAddress.Trim()) x.Title x.FirstName x.LastName )
+ 
 
     0 // return an integer exit code
